20423613b98b0ccadfdefe943091c771b6a600b9
put your github username here <muathmhawich1@gmail.com>
2024-05-08 06:21:55 +0300

    fix lab 17

--- my-app/src/components/tours/tours.js
+++ my-app/src/components/tours/tours.js
@@ -1,24 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './tours.css'; 
+import  Tour from "../tour/tour"
+
 
 const data = require("../../data/db.json");
 
 function Tours() {
     return (
         <div className="container">
+                    
+
             <div className="row">
                 {data.map((element, index) => (
-                    <div key={index} className="col-md-4 mb-4">
-                        <div className="card h-100 d-flex flex-column">
-                            <img src={element.image} className="card-img-top img-fluid" alt={element.name} />
-                            <div className="card-body flex-grow-1">
-                                <h5 className="card-title">{element.name}</h5>
-                                <div className="card-text-scroll">
-                                    <p className="card-text">{element.info}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        
+                        <Tour key={element.id} tour={element} />
+                    
                 ))}
             </div>
         </div>
